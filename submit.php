fb13bb540297c6b2c4f47131c29020ccd20e6f86
TheBrainTeam <prasunkool@gmail.com>
2016-09-23 15:48:18 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?><br>
+Your mobile number is: <?php echo $_GET["mobile"]; ?><br>
+Subject: <?php echo $_GET["subject"]; ?><br>
+Message: <?php echo $_GET["message"]; ?><br>
+
+</body>
+</html> 
